6cac8f43b45b1c6327e6272770d457b31b76bfc4
Andrey Sherstiuk <sherstukandrey15@gmail.com>
2023-04-01 15:02:03 +0300

    sadsaddsadasd

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+
+
 
 const sql = require('mssql');
 
